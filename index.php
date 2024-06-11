91cbfb1c474722902ee2d09737dc7a917a437b44
fanny-elimar <137103853+fanny-elimar@users.noreply.github.com>
2024-06-11 09:47:04 +0200

    carousel sur l'index

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require_once __DIR__ . "/templates/_header.php";
     <p>Situé en Bretagne, près de la forêt de Brocéliande, Arcadia est un lieu de découverte de la biodiversité mondiale. Au fil de votre visite, vous découvrirez des <a href="habitats.php" class="link-text">habitats</a> variés et la faune   qui y vit.</p>
 </div>
 
-<div class="bandeau-habitats d-flex justify-content-center">
+<div class="bandeau-habitats justify-content-center">
 
 <?php
 require __DIR__ . "/templates/_habitat_card.php";
@@ -21,6 +21,45 @@ require __DIR__ . "/templates/_habitat_card.php";
         En venant à Arcadia, vous aurez le plaisir de rencontrer de nombreux animaux : girafes,  éléphants,  hippopotames,  singes,  serpents,  tortues et bien d'autres encore.
     </p>
 </div>
+
+<div class="container">
+      <h1>Carrousel</h1>
+      <div id="demo" class="carousel slide carousel-fade" data-bs-ride="carousel">
+        
+
+        <!-- Carrousel -->
+        <div class="carousel-inner">
+          <div class="carousel-item active" data-interval="4000">
+            <div class="row">
+                <div class="col"><img src="<?=_ASSETS_IMAGES_FOLDER_.'jungle.webp';?>" alt="Carrousel slide 1" class="d-block"></div>
+                <div class="col"><img src="<?=_ASSETS_IMAGES_FOLDER_.'marais.webp';?>" alt="Carrousel slide 2" class="d-block"></div>
+            </div>
+          </div>
+          <div class="carousel-item" data-interval="2000">
+          <div class="row">
+                <div class="col"><img src="<?=_ASSETS_IMAGES_FOLDER_.'savane.webp';?>" alt="Carrousel slide 1" class="d-block"></div>
+                <div class="col"><img src="<?=_ASSETS_IMAGES_FOLDER_.'jungle.webp';?>" alt="Carrousel slide 2" class="d-block"></div>
+            </div>
+          </div>
+          <div class="carousel-item" data-interval="1000">
+          <div class="row">
+                <div class="col"><img src="<?=_ASSETS_IMAGES_FOLDER_.'marais.webp';?>" alt="Carrousel slide 1" class="d-block"></div>
+                <div class="col"><img src="<?=_ASSETS_IMAGES_FOLDER_.'savane.webp';?>" alt="Carrousel slide 2" class="d-block"></div>
+            </div>
+          </div>
+        </div>
+        
+        <!-- Contrôles -->
+        <a class="carousel-control-prev" href="#demo" role="button" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+
+        </a>
+        <a class="carousel-control-next" href="#demo" role="button" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+
+        </a>
+      </div>
+    </div>
 
 
 <?php require_once __DIR__ . "/templates/_footer.php"; ?>
--- templates/_header.php
+++ templates/_header.php
@@ -14,7 +14,7 @@ require_once __DIR__ . "/../lib/config.php";
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&family=Just+Another+Hand&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="assets/CSS/custom3.css">
+    <link rel="stylesheet" href="assets/CSS/custom5.css">
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
@@ -27,7 +27,7 @@ require_once __DIR__ . "/../lib/config.php";
         <header class="">
             <div class="d-flex flex-wrap align-items-end justify-content-between mt-3">
             <picture class="d-flex">
-                <source srcset="<?=_ASSETS_IMAGES_FOLDER_.'logo.png' ;?>" media="(max-width:600px)" alt="logo-arcadia" class="img-fluid img-logo">
+                <source srcset="<?=_ASSETS_IMAGES_FOLDER_.'logo.png' ;?>" media="(max-width:780px)" alt="logo-arcadia" class="img-fluid img-logo">
                 <img src="<?=_ASSETS_IMAGES_FOLDER_.'logo2.png';?>" class="img-fluid img-logo">
             </picture>    
                 <div class="text-end nav-item">
