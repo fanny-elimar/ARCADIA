1ac9e106554e891637bccf8fac60ec976b743e45
fanny-elimar <137103853+fanny-elimar@users.noreply.github.com>
2024-06-12 05:55:27 +0200

    Habitats_card update

--- templates/_habitat_card.php
+++ templates/_habitat_card.php
@@ -1,10 +1,10 @@
 <div class="card m-3" style="width: 18rem;">
-<img class="card-img-top" src="<?=_ASSETS_IMAGES_FOLDER_.'jungle.webp';?>" alt="Card image cap">
+<img class="card-img-top" src="<?=_ASSETS_IMAGES_FOLDER_.$habitat["ha_images"];?>" alt="Image <?= $habitat["ha_name"]?>">
    
 
   <div class="card-body justify-content-center">
-    <h6 class="card-title"><?= htmlentities($habitat["ha_name"])?></h5>
-    <p class="card-text"><?= htmlentities($habitat["ha_description"])?></p>
+    <h6 class="card-title"><?= ucfirst(htmlentities($habitat["ha_name"]))?></h5>
+    <p class="card-text"><?= substr(htmlentities($habitat["ha_description"]),0,200).'...'?></p>
     <a href="#" class="btn btn-primary">Voir</a>
   </div>
 </div>
