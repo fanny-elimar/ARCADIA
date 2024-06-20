b2f7faf6578e9900b18dd70c9da09f7eb0c0a493
fanny-elimar <137103853+fanny-elimar@users.noreply.github.com>
2024-06-20 09:52:28 +0200

    remarques sur l'habitat

--- lib/visit.php
+++ lib/visit.php
@@ -13,7 +13,7 @@ function addVisit(PDO $pdo, string $vi_condition, string $vi_date, string $vi_co
     return $query->execute(); 
 }
 
-function getLastCondition(PDO $pdo, int $an_id) {
+function getLastConditionByAnimalId(PDO $pdo, int $an_id) {
     $sql = "SELECT vi_condition, vi_condition_details FROM arc_visit WHERE vi_an_id = :an_id ORDER BY vi_date DESC";
     $query = $pdo->prepare($sql);
 
@@ -22,4 +22,16 @@ function getLastCondition(PDO $pdo, int $an_id) {
 
     $condition = $query->fetch(PDO::FETCH_ASSOC);
     return $condition; 
+}
+
+function addEnclosureComment(PDO $pdo, string $en_comment, int $en_id) {
+    $sql = "UPDATE arc_enclosure SET en_comment = :en_comment WHERE en_id= :en_id;";
+    $query = $pdo->prepare($sql);
+
+    $query->bindParam(':en_comment', $en_comment, PDO::PARAM_STR);
+    $query->bindParam(':en_id', $en_id, PDO::PARAM_INT);
+ 
+
+    
+    return $query->execute(); 
 }
--- templates/_vet_visit_add.php
+++ templates/_vet_visit_add.php
@@ -4,6 +4,23 @@
 $errors = [];
 $messages = [];
 
+if (isset($_POST['addEnclosureComment'])) {   /*
+    @todo ajouter la vérification sur les champs
+*/
+?>
+<!--empecher le renvoi du formulaire à l'actualisation de la page-->
+<script> location.replace(document.referrer); </script>
+<?php 
+
+$res2 = addEnclosureComment($pdo, $_POST['en_comment'], $enclosure['en_id']);
+if ($res2) {
+    $messages[] = 'Merci pour votre avis.';
+} else {
+    $errors[] = 'Une erreur s\'est produite.';
+}
+
+}
+
 if (isset($_POST['addVisit'])) {   /*
         @todo ajouter la vérification sur les champs
     */
@@ -22,15 +39,36 @@ if (isset($_POST['addVisit'])) {   /*
 }
 
 ?>
+
 <div class="container container-flux">
 <h3 class="mt-3">Compte-rendu de visite</h3>
+<div class="border p-3 rounded d-block">
+<h4>Enclos <?= $enclosure['en_name'];?></h4>
+<form name="addEnclosureComment" method="POST" class="row">
 
-<form method="POST">
+    <div class="col-6">
+<input type="text" class="form-control" id="en_comment" name="en_comment" value="<?=$enclosure['en_comment'];?>" placeholder="Remarque...">
+</div>
+<div class="col-3">
+    <input type="submit" name="addEnclosureComment" class="btn btn-primary btn-sm" value="Modifier">
+</div>
+
+            
+
+            
+
+        
+        </form>
+</div>
+
+<div class="border p-3 rounded mt-3">
+        <h4>Animal</h4>
+        <form name="addVisit" method="POST">
         <div class="mb-3 form-group row">
             <label for="vi_condition" class="col-sm-2 col-form-label">Etat</label>
             <div class="col-sm-8">
             <input type="text" class="form-control" id="vi_condition" name="vi_condition">
-</div>
+        </div>
         </div>
         <div class="mb-3 form-group row">
             <label for="vi_date" class="col-sm-2 col-form-label">Date</label>
@@ -45,9 +83,9 @@ if (isset($_POST['addVisit'])) {   /*
         </div>
         </div>
 
-        <input type="submit" name="addVisit" class="btn btn-primary btn-sm" value="Enregistrer">
+        <input type="submit" name="addVisit" class="btn btn-primary btn-sm col-2" value="Enregistrer">
 
     </form>
-
+</div>
 </div>
   
