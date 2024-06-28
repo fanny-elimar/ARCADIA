b99ae6a8072afde8e017444a7e8f8b1da665ea0a
fanny-elimar <137103853+fanny-elimar@users.noreply.github.com>
2024-06-28 05:54:11 +0200

    gestion des services en cours

--- admin/service_add.php
+++ admin/service_add.php
@@ -5,7 +5,6 @@ require_once '../lib/service.php';
 
 $messages = [];
 $errors = [];
-$error = false;
 $service = [
   'se_name' => '',
   'se_description' => '',
@@ -14,89 +13,91 @@ $service = [
 ];
 
 if (isset($_GET['id'])) {
-    $id = $_GET['id'];
+  $id = $_GET['id'];
+  $service = getServiceById($pdo, $id);
+  if (!$service) {
+    $errors[] = 'Ce service n\'existe pas';
+  }
+    $pageTitle = 'Formulaire de modification des services';
+} else {
+  $pageTitle = 'Formulaire d\'ajout de service';
+}
 
-    $service = getServiceById($pdo, $id);
-
-    if (!$service) {
-        $error = true;
-    }
-} 
-
-
-if (isset($_POST['addService'])) { ?>
-  <!--empecher le renvoi du formulaire à l'actualisation de la page-->
-
-  <?php 
-$service = [
+if (isset($_POST['addService'])) { 
+  $service = [
   'se_name' => $_POST['se_name'],
   'se_description' => $_POST['se_description'],
   'se_images' => $_POST['se_images'],
   'se_info' => $_POST['se_info']
-];
+  ];
 
-if (isset($_GET["id"])) {
-  // Avec (int) on s'assure que la valeur stockée sera de type int
-  $id = (int)$_GET["id"];
-} else {
-  $id = null;
-}
-
+  if (isset($_GET["id"])) {
+    $id = (int)$_GET["id"];
+  } else {
+    $id = null;
+  }
 
   $res = addService($pdo, $_POST['se_name'], $_POST['se_description'], $_POST['se_images'],  $_POST['se_info'], $id);
   if ($res) {
-      $messages[] = 'Le service a bien été sauvegardé';
+    $messages[] = 'Le service a bien été sauvegardé';
+    if (!isset($_GET["id"])) {
+      $service = [
+      'se_name' => '',
+      'se_description' => '',
+      'se_images' => '',
+      'se_info' => ''
+      ];
+    }
   } else {
-      $errors[] = 'Une erreur s\'est produite.';
+    $errors[] = 'Une erreur s\'est produite.';
   }
 } ?>
 
-
+<h3><?= $pageTitle ?></h3>
 <?php foreach ($messages as $message) { ?>
-    <div class="alert alert-success" role="alert">
-        <?= $message; ?>
-    </div>
+  <div class="alert alert-success" role="alert">
+    <?= $message; ?>
+  </div>
 <?php } ?>
 <?php foreach ($errors as $error) { ?>
-    <div class="alert alert-danger" role="alert">
-        <?= $error; ?>
-    </div>
-<?php } ?>
-<?php if (!$error) { ?>
-
-      <div >
-        <form method="POST">
-        <div class="form-group row">
-            <label for="se_name" class="col-sm-2 col-form-label mb-3">Service</label>
-            <div class="col-sm-8">
-            <input type="text" class="form-control" id="se_name" name="se_name" value="<?= $service['se_name'];?>">
+  <div class="alert alert-danger" role="alert">
+    <?= $error; ?>
+  </div>
+<?php } 
+if ($service) { ?>
+  <div >
+    <form method="POST">
+      <div class="form-group row">
+        <label for="se_name" class="col-sm-2 col-form-label mb-3">Service</label>
+        <div class="col-sm-8">
+          <input type="text" class="form-control" id="se_name" name="se_name" value="<?= $service['se_name'];?>">
         </div>
-        <div class="form-group row">
-            <label for="se_description" class="col-sm-2 col-form-label">Description</label>
-            <div class="col-sm-8">
-            <textarea type="text" class="form-control  mb-3" id="se_description" name="se_description" rows=7 ><?= $service['se_description'];?></textarea>
+      </div>
+      <div class="form-group row">
+        <label for="se_description" class="col-sm-2 col-form-label">Description</label>
+        <div class="col-sm-8">
+          <textarea type="text" class="form-control  mb-3" id="se_description" name="se_description" rows=7 ><?= $service['se_description'];?></textarea>
         </div>
-        <div class="form-group row">
-            <label for="se_info" class="col-sm-2 col-form-label">Détails</label>
-            <div class="col-sm-8">
-            <textarea type="text" class="form-control  mb-3" id="se_info" name="se_info" rows=5><?= $service['se_info'];?></textarea>
+      </div>
+      <div class="form-group row">
+        <label for="se_info" class="col-sm-2 col-form-label">Détails</label>
+        <div class="col-sm-8">
+          <textarea type="text" class="form-control  mb-3" id="se_info" name="se_info" rows=5><?= $service['se_info'];?></textarea>
         </div> 
-        <div class="form-group row">
-            <label for="se_images" class="col-sm-2 col-form-label mb-3">Image</label>
-            <div class="col-sm-8">
-            <input type="text" class="form-control" id="se_images" name="se_images">
+      </div>
+      <div class="form-group row">
+        <label for="se_images" class="col-sm-2 col-form-label mb-3">Image</label>
+        <div class="col-sm-8">
+          <input type="text" class="form-control" id="se_images" name="se_images">
         </div>
-        <div class="form-group row">
+      </div>
+      <div class="form-group row d-flex justify-content-center">
         <input type="submit" name="addService" class="btn btn-primary btn-sm col-2 mb-3" value="Valider">
-        </div>
-        </form>
-        </div>
-
-          
-<?php } else { ?>
-  <h1>Page introuvable</h1>
+      </div>
+    </form>
+  </div>
 <?php } ?>
-</div>
+
 
 <?php
 require_once "templates/_footer.php";
--- admin/services.php
+++ admin/services.php
@@ -6,52 +6,51 @@ require_once '../lib/service.php';
 $services=getServices($pdo);
 $messages =[];
 $errors =[];
-
 ?>
-<div class="px-4 text-left" >
-  <h2 class="display-5">Gestion des services</h2>
-</div>
+
+<h3 class="display-5">Gestion des services</h3>
 
 <?php foreach ($messages as $message) { ?>
-        <div class="alert alert-success" role="alert">
-            <?= $message; ?>
-        </div>
-    <?php } ?>
-    <?php foreach ($errors as $error) { ?>
-        <div class="alert alert-danger" role="alert">
-            <?= $error; ?>
-        </div>
-    <?php } ?>
+    <div class="alert alert-success" role="alert">
+        <?= $message; ?>
+    </div>
+<?php } ?>
+<?php foreach ($errors as $error) { ?>
+    <div class="alert alert-danger" role="alert">
+        <?= $error; ?>
+    </div>
+<?php } ?>
 
 <div class="">
     <?php foreach ($services as $service) {
         if (isset($_POST['deleteService'.$service['se_id']])) {?>
             <script> location.replace(document.referrer); </script>
-  <?php
+            <?php
             $res = deleteService($pdo, $service['se_id']);
             if ($res) {
                 $messages[] = 'Le service a bien été sauvegardé';
             } else {
                 $errors[] = 'Une erreur s\'est produite.';
             }
-          } ?>
-        
-        
-       <div class="row d-flex mb-0">
-            <div class="col-4"><p class="" ><?= ucfirst(htmlentities($service["se_name"]))?></p></div>
-            <div class="col"><a class="btn btn-primary btn-sm p-1" href="<?="service_add.php?id=".$service['se_id']?>">Modifier</a></div>
-            <form method="POST">
-                <input type="submit" name="deleteService<?= $service['se_id']?>" class="btn btn-primary btn-sm col-2 mb-3" value="Supprimer">
-            </form>
-            
-       
-       </div>
-        <?php } ?>
-        
-
+        } ?>
+        <div class="row d-flex mb-0">
+            <div class="col-4">
+                <p class="" ><?= ucfirst(htmlentities($service["se_name"]))?></p>
+            </div>
+            <div class="col">
+                <a class="btn btn-primary btn-sm p-1" href="<?="service_add.php?id=".$service['se_id']?>">Modifier</a>
+            </div>
+            <div class="col">
+                <form method="POST">
+                    <input type="submit" name="deleteService<?= $service['se_id']?>" class="btn btn-primary btn-sm mb-3" value="Supprimer" onclick="return confirm('Êtes-vous sûr de vouloir supprimer cet article ?')">
+                </form>
+            </div>
+        </div>
+    <?php } ?>
 </div>
 <a href="service_add.php" class="btn btn-primary btn-sm">Ajouter un service</a>
 </div>
+
 
 <?php
 require_once '../templates/_footer.php'
