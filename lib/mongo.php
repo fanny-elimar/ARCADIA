ddf12d96c5d99c151c515e32fac54f6ae7bbed8d
fanny-elimar <137103853+fanny-elimar@users.noreply.github.com>
2024-09-05 15:53:26 +0200

    config mongodb

--- lib/mongo.php
+++ lib/mongo.php
@@ -1,7 +1,7 @@
 <?php 
 require_once $_SERVER['DOCUMENT_ROOT']."/ARCADIA/vendor/autoload.php";
 
-if (getenv('mongoDB') !== false) {
+if (getenv('MONGODB_URI') !== false) {
     $client = new MongoDB\Client("mongodb+srv://fannyelimar:elimarmdpatlas@cluster1.lqawy.mongodb.net/");
 } else {
     $client = new MongoDB\Client("mongodb://localhost:27017");
