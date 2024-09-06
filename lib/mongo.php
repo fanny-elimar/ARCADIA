7db176ae53cc13e349ccf79804c0c70a23dafd88
fanny-elimar <137103853+fanny-elimar@users.noreply.github.com>
2024-09-06 14:25:23 +0200

    config mongodb

--- lib/mongo.php
+++ lib/mongo.php
@@ -1,10 +1,12 @@
 <?php 
 //require_once $_SERVER['DOCUMENT_ROOT']."/ARCADIA/vendor/autoload.php";
-require_once  "./vendor/autoload.php";
-var_dump($_SERVER['DOCUMENT_ROOT']);
+//require_once  "./vendor/autoload.php";
+//var_dump($_SERVER['DOCUMENT_ROOT']);
 
 if (getenv('MONGODB_URI') !== false) {
+    require_once $_SERVER['DOCUMENT_ROOT']."/vendor/autoload.php";
     $client = new MongoDB\Client("mongodb+srv://fannyelimar:elimarmdpatlas@cluster1.lqawy.mongodb.net/");
 } else {
+    require_once $_SERVER['DOCUMENT_ROOT']."/ARCADIA/vendor/autoload.php";
     $client = new MongoDB\Client("mongodb://localhost:27017");
 }
