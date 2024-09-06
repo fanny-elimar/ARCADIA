a173a5df015f9f52717d75ca008f55f62df58927
fanny-elimar <137103853+fanny-elimar@users.noreply.github.com>
2024-09-06 13:53:17 +0200

    config mongodb

--- lib/mongo.php
+++ lib/mongo.php
@@ -1,8 +1,8 @@
 <?php 
 require_once $_SERVER['DOCUMENT_ROOT']."/ARCADIA/vendor/autoload.php";
 
-if (getenv('MONGODB_URI') !== false) {
+//if (getenv('MONGODB_URI') !== false) {
     $client = new MongoDB\Client("mongodb+srv://arcadiauser:AuNyMxJ5ByDNIvqDd@cluster1.lqawy.mongodb.net/");
-} else {
-    $client = new MongoDB\Client("mongodb://localhost:27017");
-}
+//} else {
+//    $client = new MongoDB\Client("mongodb://localhost:27017");
+//}
