09bcdd3b0e5a48f19e1106539af3a2f97c3bb473
fanny-elimar <137103853+fanny-elimar@users.noreply.github.com>
2025-01-14 10:57:23 +0100

    gestion erreur pdo

--- lib/pdo.php
+++ lib/pdo.php
@@ -3,8 +3,23 @@
 try
 {
     $pdo = new PDO("pgsql:dbname="._DB_NAME_." options='--client_encoding=UTF8';host="._DB_SERVER_.";", _DB_USER_, _DB_PASSWORD_);
-}
-catch (Exception $e)
-{
-    die('Erreur : ' . $e->getMessage());
+// Définir le mode d'erreur de PDO pour lancer des exceptions
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    if (getenv('alwaysdata') !== false) {
+        // Retourner un message générique à l'utilisateur
+    echo "Impossible de se connecter à la base de données. Veuillez réessayer plus tard.";
+    exit; // Arrêter le script après l'erreur
+        
+    } else {
+        // Loguer l'erreur dans un fichier de log
+        $logMessage = date('Y-m-d H:i:s') . ' - ' . $e->getMessage() . "\n";
+    error_log($logMessage, 3, 'logs/arcadia_database_errors.log');
+
+            // Retourner un message générique à l'utilisateur
+            echo "Impossible de se connecter à la base de données. Veuillez réessayer plus tard.";
+            exit; // Arrêter le script après l'erreur
+    }
+    
+    
 }
